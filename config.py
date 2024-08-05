29c69f307be87a025a4c69f391a601f1daaa0d1a
ams6699 <116522993+ams6699@users.noreply.github.com>
2024-08-05 19:02:01 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+API_KEY = 'Your Bybit API KEY'
+API_SECRET = 'Your Bybit API Secret'
+SYMBOL = 'XRPUSDT'
+TARGET_PRICE = 0.2  # User-defined target price
+QTY = 1  # Quantity to sell
+TIME_CHECK=20 #Sleep Time
